5ad0042c2e6778a291b1b08cc27186d080b29591
Mathias Moeller <matze.moeller@gmail.com>
2016-12-09 18:28:56 +0100

    Add mock data

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
 var lastNodeId = 0;
-const nodeOptions = {
+var counter = 0;
+
+var nodeOptions = {
   font: '12px Roboto white',
   color: 'white',
   shape: 'circularImage',
@@ -24,7 +26,7 @@ var options = {
   height: '100%',
   width: '100%',
   physics: {
-    maxVelocity: 10,
+    maxVelocity: 25,
     repulsion: {
       nodeDistance: 1000,
       springLength: 1000,
@@ -48,33 +50,71 @@ var network = new vis.Network(container, data, options);
 network.on('click', clickHandler);
 
 function clickHandler(event) {
-  nodes.add(Object.assign({
-    id: ++lastNodeId,
-    label: 'Marlon Brando',
-    image: 'http://a1.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,q_80,w_620/MTE4MDAzNDEwNDYyMjEzNjQ2.jpg'
-  }, nodeOptions));
-  edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Actor'});
+  counter++;
+  if (counter == 1) {
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'Marlon Brando',
+      image: 'http://a1.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,q_80,w_620/MTE4MDAzNDEwNDYyMjEzNjQ2.jpg'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Actor'});
 
-  nodes.add(Object.assign({
-    id: ++lastNodeId,
-    label: 'Francis Ford Coppola',
-    image: 'https://www.nyfa.edu/student-resources/wp-content/uploads/2014/06/Francis-Ford-Coppola.jpg'
-  }, nodeOptions));
-  edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Director'});
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'Francis Ford Coppola',
+      image: 'https://www.nyfa.edu/student-resources/wp-content/uploads/2014/06/Francis-Ford-Coppola.jpg'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Director'});
 
-  nodes.add(Object.assign({
-    id: ++lastNodeId,
-    label: 'Martin Sheen',
-    image: 'http://www.onbeing.org/sites/default/files/styles/width175/public/MARTIN-SHEEN.gif'
-  }, nodeOptions));
-  edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Actor'});
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'Martin Sheen',
+      image: 'http://www.onbeing.org/sites/default/files/styles/width175/public/MARTIN-SHEEN.gif'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Actor'});
 
-  nodes.add(Object.assign({
-    id: ++lastNodeId,
-    label: 'Harrison Ford',
-    image: 'http://i1076.photobucket.com/albums/w450/tincanbandit/Movie%20guns/42218-star-wars_833648f8_zpsf47a6e46.jpg'
-  }, nodeOptions));
-  edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Actor'});
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'Harrison Ford',
+      image: 'http://i1076.photobucket.com/albums/w450/tincanbandit/Movie%20guns/42218-star-wars_833648f8_zpsf47a6e46.jpg'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Actor'});
+
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'Full Metal Jacket',
+      image: 'https://resizing.flixster.com/fsPpfxtGdLMbLjJvvPqFiNX7uG8=/180x270/v1.bTsxMTE2ODAyOTtqOzE3MjQ4OzIwNDg7ODAwOzEyMDA'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Recommendation'});
+  } else if (counter == 2) {
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'The Godfather',
+      image: 'https://i.ytimg.com/vi/rt-r-w7Z2Ag/maxresdefault.jpg'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Movie'});
+
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'The Godfather Part II',
+      image: 'http://la-screenwriter.com/wp-content/uploads/2012/02/936full-the-godfather-part-ii-poster.jpg'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Movie'});
+
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'The Rainmaker',
+      image: 'https://resizing.flixster.com/nDSnVAINbKpCvSHmEfsbucslVqc=/206x305/v1.bTsxMTIwNDg0NDtqOzE3MjI5OzEyMDA7OTAwOzEyMDA'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Movie'});
+
+    nodes.add(Object.assign({
+      id: ++lastNodeId,
+      label: 'The Great Gatsby',
+      image: 'http://static.rogerebert.com/uploads/movie/movie_poster/the-great-gatsby-1974/large_70FxgKoE8UXDYc8IZL0b6rMvNFD.jpg'
+    }, nodeOptions));
+    edges.add({from: event.nodes[0], to: lastNodeId, font: fontOptions, label: 'Movie'});
+  }
 }
 
 function search(event) {
