66248e32e5b7116a39bc344ef870bc360d737926
Mathias Moeller <matze.moeller@gmail.com>
2017-01-02 16:00:46 +0100

    Allow images to be toggled on and off

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ function clickHandler(event) {
     case 'movie':
       expandMovie(nodeID);
       break;
+
     case 'director':
       expandDirector(nodeID);
       break;
@@ -86,20 +87,26 @@ function addNode(id, name, group) {
   }
 }
 
+function useImages() {
+  return document.getElementById('imageSwitch').checked;
+}
+
 function findImage(nodes, nodeID, query) {
-  // TODO: what is the optimal image size (pithumbsize)?
-  get('https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&pithumbsize=50&titles=' + query)
-    .then(function(response) {
-      response.json().then(data => {
-        try {
-          let pages = data.query.pages;
-          let url = pages[Object.keys(pages)].thumbnail.source;
-          nodes.update({id: nodeID, image: url});
-        } catch (e) {
-          console.log(e, data);
-        }
+  if (useImages()) {
+    // TODO: what is the optimal image size (pithumbsize)?
+    get('https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&pithumbsize=50&titles=' + query)
+      .then(function(response) {
+        response.json().then(data => {
+          try {
+            let pages = data.query.pages;
+            let url = pages[Object.keys(pages)].thumbnail.source;
+            nodes.update({id: nodeID, image: url});
+          } catch (e) {
+            console.log(e, data);
+          }
+        })
       })
-    })
+  }
 }
 
 function showToast(message) {
@@ -143,8 +150,15 @@ function initialize() {
     dialogPolyfill.registerDialog(dialog);
   }
 
+  document.getElementById('imageSwitch').addEventListener('change', resetImages);
 }
 
-window.onload = function() {
-  initialize();
-};
+function resetImages(event) {
+  if (event.srcElement.checked) {
+    addImages();
+  } else {
+    removeImages();
+  }
+}
+
+initialize();
